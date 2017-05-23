0e6b0100fea4db8d8d6715e0ed9deae9358ef60b
110crosswalk <avg.holdingsllc@gmail.com>
2017-05-23 08:19:46 -0400

    Update calculator.c
    review my comments

--- calculator.c
+++ calculator.c
@@ -5,23 +5,27 @@ int main()
 {
     int n, num1,num2,result;
 
-
-printf("what operatoin do you want to do?\n");
-printf("press 1 to add\n");
-printf("press 2 to subtract\n");
-scanf("%d",&n);
-printf("please enter number\n");
-scanf("%d",&num1);
-printf("second number\n");
-scanf("%d",&num2);
-switch(n)
-{
-    case 1: result=num1+num2;
-    printf("%d\n",result);
-    break;
-    case 2: result=num1-num2;
-    printf("%d",result);
-    break;
-}
+    printf("what operatoin do you want to do?\n");      //you need put indintation in
+    printf("press 1 to add\n");                         //you can combine three printfs in to one
+    printf("press 2 to subtract\n");
+    scanf("%d",&n);
+    
+    printf("please enter number\n");                    //separate logic by empty lines
+    scanf("%d",&num1);
+    
+    printf("second number\n");
+    scanf("%d",&num2);
+    
+    switch(n)
+    {
+        case 1: 
+            result=num1+num2;                           //some people jem everything togather like this line
+            printf("%d\n",result);                      //i think puting a spaces makes it more readable
+        break;
+        case 2: 
+            result = num1 - num2;                           //like this
+            printf("%d", result);
+        break;
+    }
   return 0;
 }
